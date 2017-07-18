b49bfe0a2848b6eb232963325531dac324ea807f
Nalipp <nateplipp@gmail.com>
2017-07-18 15:26:07 +0900

    http calls with fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": "1", "firstName": "nate", "lastName": "lip", "email": "m@m.c"},
+    {"id": "2", "firstName": "dale", "lastName": "seo", "email": "m@m.c"},
+    {"id": "3", "firstName": "ben", "lastName": "sdck", "email": "m@m.c"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log('up and running on : ' + port);
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,3 @@
 import chalk from 'chalk'; // allows you to color code you messages
 
 console.log(chalk.green('Starting app in dev mode'));// eslint-disable-line no-console
- 
