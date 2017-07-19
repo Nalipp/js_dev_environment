d7803045957c9a4ea7648a6ebf87c27868b3a2ab
Nalipp <nateplipp@gmail.com>
2017-07-19 12:45:24 +0900

    seperate ui and api hosting with heroku and surge

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://calm-springs-19374.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
